28175dab18b4f195e9f79ea42c320cb27da58d85
EX1ST3NCE <prasadnitin05@gmail.com>
2021-02-18 01:34:57 +0530

    Initial Trees for Crdroid

---
+++ AndroidProducts.mk
@@ -0,0 +1,13 @@
+#
+# Copyright (C) 2019 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/crdroid_raphael.mk
+
+COMMON_LUNCH_CHOICES := \
+    crdroid_raphael-user \
+    crdroid_raphael-userdebug \
+    crdroid_raphael-eng
